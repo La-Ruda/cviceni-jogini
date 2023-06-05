c5b0535bd27835cdbf8373bb9970d8c60a8e5d57
La-Ruda <herempen@gmail.com>
2023-06-05 14:28:09 +0200

    commit

---
+++ Pose/script.js
@@ -0,0 +1,3 @@
+export const Pose = (props) => {
+    return `<img class="pose" src=img/yoga-pose.jpg>`
+}
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('funguju!');
+console.log('Spirit of this Machine, heed my will!');
+
+import { Header } from "./Header/script.js";
+
+import { Intro } from "./Intro/script.js";
+
+import { Pose } from "./Pose/script.js";
+
+
+const contentWrapper = document.querySelector(".app-container");
+
+contentWrapper.innerHTML = Header() + Intro() + Pose();
